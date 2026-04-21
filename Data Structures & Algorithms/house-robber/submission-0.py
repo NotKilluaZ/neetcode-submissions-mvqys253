92d442c3f6d7db9ace9b8e2a0da1c40b65ea0487
Charlie Parker <129253904+NotKilluaZ@users.noreply.github.com>
2026-04-21 08:22:25 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        one = 0
+        two = 0
+
+        for n in nums:
+            temp = max(one + n, two)
+            one = two
+            two = temp
+
+        return two
